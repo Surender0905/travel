64e7dd7873dc0a074a632a4e9d45043c5e54f674
Surender Pratap Singh <rk31151@gmail.com>
2024-03-29 13:45:30 +0530

    deploy check

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const EditHotel = () => {
   return <div>EditHotel</div>;
 };
--- frontend/src/pages/MyBookings.tsx
+++ frontend/src/pages/MyBookings.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const MyBookings = () => {
   return <div>MyBookings</div>;
 };
