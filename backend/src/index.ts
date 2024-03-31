323eed05cac886ce0089da4948fd44cb20c2d75a
Surender Pratap Singh <rk31151@gmail.com>
2024-03-31 21:09:26 +0530

    seach ui done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRouter from './routes/user';
 import authRouter from './routes/auth';
+import hotelRoutes from './routes/hotels';
 import myHotelRoutes from './routes/my-hotels';
 import cookieParser from 'cookie-parser';
 import path from 'path';
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/my-hotels', myHotelRoutes);
+app.use('/api/hotels', hotelRoutes);
 
 app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppContextProvider } from './contexts/AppContext.tsx';
+import { SearchContextProvider } from './contexts/SearchContext.tsx';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +19,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
-        <App />
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
       </AppContextProvider>
 
       <ReactQueryDevtools initialIsOpen={false} />
